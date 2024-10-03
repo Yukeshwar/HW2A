14d711bb69dc92b0d2d479684d0b4b5179f73e9a
Yukeshwar <123797754+Yukeshwar@users.noreply.github.com>
2024-10-02 23:29:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
+//Artist inspried from is kazimir malevich
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(4000, 4000);
+  background(220);
+  fill(0); // Black color
+  rect(50, 100, 150, 30);
+  fill(0, 128, 0); // Green color
+  rect(80, 150, 120, 20);
+  fill(0, 0, 255); // Blue color
+  triangle(230, 200, 180, 250, 280, 250);
+  fill(255, 255, 0); // Yellow color
+  rect(160, 280, 50, 20);
+  
 }
 
 function draw() {
+  
 }
